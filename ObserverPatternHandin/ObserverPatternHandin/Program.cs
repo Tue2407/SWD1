ebe3b498040d8209e822e865a4cb5f0cb99b2333
Emil <pehmil@gmail.com>
2018-02-21 11:59:05 +0100

    Further work on Stock class

--- ObserverPatternHandin/ObserverPatternHandin/Program.cs
+++ ObserverPatternHandin/ObserverPatternHandin/Program.cs
@@ -10,23 +10,27 @@ namespace ObserverPatternHandin
     {
         static void Main(string[] args)
         {
+
         }
     }
 
     public class Stock
     {
         public string _ticker { set; get; }
-        public int _value { set; get; }
+        public float _value { set; get; }
 
-        public Stock(string ticker, int value)
+        public Stock(string ticker, float value)
         {
             _ticker = ticker;
             _value = value;
         }
 
-        public void setStockValue()
+        public void changeStockValue(string ticker, float value)
         {
-
+            Console.WriteLine("Enter ticker symbol: ");
+            _ticker = Console.ReadLine();
+            Console.WriteLine("Enter stock value: ");
+            _value = Convert.ToInt32(Console.ReadLine());
         }
     }
 
