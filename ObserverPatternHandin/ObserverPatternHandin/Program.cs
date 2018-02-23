8acdaf432aad3f2c81aba6e12705318f5d1f7be3
Pehmil <pehmil@gmail.com>
2018-02-23 13:04:23 +0100

    test

--- ObserverPatternHandin/ObserverPatternHandin/Program.cs
+++ ObserverPatternHandin/ObserverPatternHandin/Program.cs
@@ -71,7 +71,7 @@ namespace ObserverPatternHandin
             Portfolio obj2 = new Portfolio();
             obj2.stockList = new List<Stock>();
             obj2.stockList.Add(obj1);
-
+            //test
             //Insert stocks
             Console.WriteLine("Dine stocks:");
             //Udskriv alle stocks
