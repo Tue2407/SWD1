66d30a780c59d23f54cf0c1f31f614f98dc1d885
Daniel Hansen <dph1108@gmail.com>
2018-02-23 14:59:39 +0100

    Genskabte vores system

--- ObserverPatternHandin/ObserverPatternHandin/Display.cs
+++ ObserverPatternHandin/ObserverPatternHandin/Display.cs
@@ -14,12 +14,15 @@ namespace ObserverPatternHandin
             Portfolio obj2 = new Portfolio();
             obj2.stockList = new List<Stock>();
             obj2.stockList.Add(obj1);
-            //test23
+
             //Insert stocks
             Console.WriteLine("Dine stocks:");
             //Udskriv alle stocks
             obj2.stockList.ForEach(i => Console.WriteLine("{0}\t", i));
         }
-        //Test somedude
+
+
+        
+
     }
 }
--- ObserverPatternHandin/ObserverPatternHandin/Program.cs
+++ ObserverPatternHandin/ObserverPatternHandin/Program.cs
@@ -38,31 +38,9 @@ namespace ObserverPatternHandin
 
     public interface IObserver
     {
-<<<<<<< HEAD
-        public List<Stock> stockList = new List<Stock>();
-        //testcomment
 
-        public Portfolio()
-        { }
-    }
-
-    public class PortfolioDisplay
-    {
-        public void Display()
-        {
-            Stock obj1 = new Stock();
-            Portfolio obj2 = new Portfolio();
-            obj2.stockList = new List<Stock>();
-            obj2.stockList.Add(obj1);
-
-            //Insert stocks
-            Console.WriteLine("Dine stocks:");
-            //Udskriv alle stocks
-            obj2.stockList.ForEach(i => Console.WriteLine("{0}\t", i));
-        }
-        //Test somedude
-=======
         void Update(string name, float value);
->>>>>>> fcc7f2912015f6d69e830b33af3c9f396463fbcc
     }
+
+
 }
