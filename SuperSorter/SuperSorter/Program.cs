0dfbc73a485b59f4dd98878d33f6c7396e9412ce
Daniel Hansen <dph1108@gmail.com>
2018-02-28 10:32:27 +0100

    Added Lab exercise SuperSorter

---
+++ SuperSorter/SuperSorter/Program.cs
@@ -0,0 +1,23 @@
+﻿//Opgaven handler om at bruge forskellige metoder at lave den samme sortering. Vi kan sortere med forskellige
+//algoritmer. I stedet for at give det hele forskellige klasser og metoder, så kan vi bare bruge en abstrakt
+//klasse som ændre sig med hvad man bruger.
+//Steps:
+//Lav en generel sammenhængende klasse (Noget de forskellige algoritmer har tilsammen)
+//Lav implementering som abstrakte klasser
+//Afprøv de forskellige klasser i main (Polymorfi)
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperSorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
